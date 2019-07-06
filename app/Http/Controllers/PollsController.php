934e7532fab52bef285302dcaed356ef79fcfdb2
Fatemeh Barati <fatemehbarati@gmail.com>
2019-07-06 12:50:57 +1000

    add api route for getting a specific poll by id

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Poll;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class PollsController extends Controller
 {
+    /**
+     * return json array of all Polls
+     * @return JsonResponse
+     */
     public function index()
     {
         return response()->json(Poll::get(), 200);
+    }
+
+    /**
+     * return json object for a specific Poll
+     * @param $id
+     * @return JsonResponse
+     */
+    public function show($id)
+    {
+        return response()->json(Poll::find($id), 200);
     }
 }
